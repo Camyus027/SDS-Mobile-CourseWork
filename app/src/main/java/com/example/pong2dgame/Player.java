53e27c284a67936be2e360e3134f76e23ff724d6
Camyus <lm.camino.2020@alumnos.urjc.es>
2023-09-27 11:38:33 +0300

    Create player class
    A player class have been crated. For now, players have:
    - A name
    - Their record
    - A meassure for their raquect
    - A paint object to put obejcts in the screen
    - A bound object to avoid collitions when using the raquect

---
+++ app/src/main/java/com/example/pong2dgame/Player.java
@@ -0,0 +1,87 @@
+package com.example.pong2dgame;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+public class Player {
+
+    private String name;
+
+    private int racquetWidth;
+    private int racquetHeight;
+    private int score;
+    private Paint paint;
+    private RectF bounds; // To prevent collisions
+
+    public Player(int racquetWidth, int racquetHeight, Paint paint) {
+        this.racquetWidth = racquetWidth;
+        this.racquetHeight = racquetHeight;
+        this.paint = paint;
+        score = 0;
+        bounds = new RectF(0,0,racquetWidth,racquetHeight);
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawRoundRect(bounds,5,5, paint);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "score=" + score +
+                '}' + "Width: " + racquetWidth
+                + "Height: " + racquetHeight
+                ;
+    }
+
+    /* GETTERS AND SETTERS */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRacquetWidth() {
+        return racquetWidth;
+    }
+
+    public void setRacquetWidth(int racquetWidth) {
+        this.racquetWidth = racquetWidth;
+    }
+
+    public int getRacquetHeight() {
+        return racquetHeight;
+    }
+
+    public void setRacquetHeight(int racquetHeight) {
+        this.racquetHeight = racquetHeight;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Paint getPaint() {
+        return paint;
+    }
+
+    public void setPaint(Paint paint) {
+        this.paint = paint;
+    }
+
+    public RectF getBounds() {
+        return bounds;
+    }
+
+    public void setBounds(RectF bounds) {
+        this.bounds = bounds;
+    }
+}
